45fc86a07d64c55d3bb44ab524682bc3274f3c5b
ConfuzzledDuck <jonathon@flutt.co.uk>
2014-06-07 08:48:13 +0000

    Added Excel/CSV conversion.

---
+++ Excel2PDF.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ConvertAPI;
+
+require_once('Abstract2PDF.php');
+
+ /**
+  * Extends the ConvertAPI class to convert Exel documents into PDF format via
+  * convertapi.com.
+  *
+  * @see http://www.convertapi.com/excel-pdf-api
+  */
+class Excel2Pdf extends Abstract2Pdf {
+
+ /**
+  * URL of the appropriate convertapi.com API.
+  */
+	protected $_apiUrl = ' http://do.convertapi.com/Excel2Pdf';
+
+ /**
+  * An array of valid input file formats for this conversion. Overrides the
+  * parent array.
+  */
+	protected $_validInputFormats = array('csv', 'xls', 'xlsb',
+	                                      'xlsx', 'xlt', 'xltx');
+
+}
