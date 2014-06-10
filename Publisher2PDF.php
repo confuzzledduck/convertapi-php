c684854509de01115316e2460b7a5f13eb4a6cf9
ConfuzzledDuck <jonathon@flutt.co.uk>
2014-06-10 16:51:02 +0000

    Added Publisher conversion.

---
+++ Publisher2PDF.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ConvertAPI;
+
+require_once('Abstract2PDF.php');
+
+ /**
+  * Extends the ConvertAPI class to convert Microsoft Publisher documents into
+  * PDF format via convertapi.com.
+  *
+  * @see http://www.convertapi.com/publisher-pdf-api
+  */
+class Publisher2Pdf extends Abstract2Pdf {
+
+ /**
+  * URL of the appropriate convertapi.com API.
+  */
+	protected $_apiUrl = 'http://do.convertapi.com/Publisher2Pdf';
+
+ /**
+  * An array of valid input file formats for this conversion. Overrides the
+  * parent array.
+  */
+	protected $_validInputFormats = array('pub');
+
+}
