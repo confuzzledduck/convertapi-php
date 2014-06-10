99d9bac873b6b2490c73c089bab7d54d3800f1c7
ConfuzzledDuck <jonathon@flutt.co.uk>
2014-06-10 16:50:21 +0000

    Added Post Script conversion.

---
+++ PostScript2PDF.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ConvertAPI;
+
+require_once('Abstract2PDF.php');
+
+ /**
+  * Extends the ConvertAPI class to convert postscript documents into PDF format
+  * via convertapi.com.
+  *
+  * @see http://www.convertapi.com/postscript-pdf-api
+  */
+class PostScript2Pdf extends Abstract2Pdf {
+
+ /**
+  * URL of the appropriate convertapi.com API.
+  */
+	protected $_apiUrl = 'http://do.convertapi.com/PostScript2Pdf';
+
+ /**
+  * An array of valid input file formats for this conversion. Overrides the
+  * parent array.
+  */
+	protected $_validInputFormats = array('ps', 'eps', 'prn');
+
+}
