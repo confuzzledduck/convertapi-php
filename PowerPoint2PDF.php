b97a063b2163710cfe1aaba9eb8571549e073211
ConfuzzledDuck <jonathon@flutt.co.uk>
2014-06-10 16:50:37 +0000

    Added PowerPoint conversion.

---
+++ PowerPoint2PDF.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ConvertAPI;
+
+require_once('Abstract2PDF.php');
+
+ /**
+  * Extends the ConvertAPI class to convert PowerPoint documents into PDF format
+  * via convertapi.com.
+  *
+  * @see http://www.convertapi.com/powerpoint-pdf-api
+  */
+class PowerPoint2Pdf extends Abstract2Pdf {
+
+ /**
+  * URL of the appropriate convertapi.com API.
+  */
+	protected $_apiUrl = 'http://do.convertapi.com/PowerPoint2Pdf';
+
+ /**
+  * An array of valid input file formats for this conversion. Overrides the
+  * parent array.
+  */
+	protected $_validInputFormats = array('pot', 'potx', 'pps',
+	                                      'ppsx', 'ppt', 'pptx');
+
+}
