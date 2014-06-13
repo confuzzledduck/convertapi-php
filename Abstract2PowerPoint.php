a38fefb65fb28b4ddc6bb5642f469f0954f26733
ConfuzzledDuck <jonathon@flutt.co.uk>
2014-06-13 16:13:38 +0000

    Added conversion PDF to PowerPoint.

---
+++ Abstract2PowerPoint.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ConvertAPI;
+
+require_once('ConvertAPI.php');
+
+ /**
+  * Extends the ConvertAPI class to convert various documents into PowerPoint
+  * format via convertapi.com.
+  */
+abstract class Abstract2PowerPoint extends ConvertAPI {
+
+ /* Magic methods. */
+
+ /**
+  * Magic setter method. Checks and sets values for $_additionalParameters.
+  *
+  * @param string $name Name of the additional parameter to set.
+  * @param string $value Value to set the parameter to.
+  */
+	public function __set($name, $value) {
+
+		switch ($name) {
+			case 'StoreFile':
+				if (is_bool($value)) {
+					$this->_additionalParameters[$name] = $value;
+				} else {
+					throw new \Exception($name.' must be a boolean value.');
+				}
+			break;
+			case 'Timeout':
+				if (is_int($value) && $value >= 5 && $value <= 600) {
+					$this->_additionalParameters[$name] = $value;
+				} else {
+					throw new \Exception($name.' must be an integer between 5 and 600.');
+				}
+			break;
+		}
+
+	}
+
+}
---
+++ Pdf2PowerPoint.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ConvertAPI;
+
+require_once('Abstract2PowerPoint.php');
+
+ /**
+  * Extends the ConvertAPI class to convert PDF documents into PowerPoint
+  * format via convertapi.com.
+  *
+  * @see http://convertapi.com/pdf-powerpoint-api
+  */
+class Pdf2PowerPoint extends Abstract2PowerPoint {
+
+ /**
+  * URL of the appropriate convertapi.com API.
+  */
+	protected $_apiUrl = 'http://do.convertapi.com/Pdf2PowerPoint';
+
+ /**
+  * An array of valid input file formats for this conversion. Overrides the
+  * parent array.
+  */
+	protected $_validInputFormats = array('pdf', 'pdfa');
+
+}
