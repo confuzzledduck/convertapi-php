7ccd7445cebf8cd5618e133485df068c8a631f42
ConfuzzledDuck <jonathon@flutt.co.uk>
2014-06-16 16:32:01 +0000

    Added PDF to image conversion.

---
+++ Pdf2Image.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ConvertAPI;
+
+require_once('Abstract2Image.php');
+
+ /**
+  * Extends the ConvertAPI class to convert PDF documents into image format via
+  * convertapi.com.
+  *
+  * @see http://convertapi.com/pdf-image-api
+  */
+class Pdf2PowerPoint extends Abstract2PowerPoint {
+
+ /**
+  * URL of the appropriate convertapi.com API.
+  */
+	protected $_apiUrl = 'http://do.convertapi.com/Pdf2Image';
+
+ /**
+  * An array of valid input file formats for this conversion. Overrides the
+  * parent array.
+  */
+	protected $_validInputFormats = array('pdf');
+
+}
