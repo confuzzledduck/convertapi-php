5c14259097184d02d40341d1b364195979648ce4
ConfuzzledDuck <jonathon@flutt.co.uk>
2014-06-21 07:43:11 +0000

    Added conversion from Web to PDF.

---
+++ Web2PDF.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ConvertAPI;
+
+require_once('Abstract2PDF.php');
+
+ /**
+  * Extends the ConvertAPI class to convert URLs into PDF format via
+  * convertapi.com.
+  *
+  * @see http://www.convertapi.com/web-pdf-api
+  */
+class Web2Pdf extends Abstract2Pdf {
+
+ /**
+  * URL of the appropriate convertapi.com API.
+  */
+	protected $_apiUrl = 'http://do.convertapi.com/Web2Pdf';
+
+ /**
+  * An string indicating that the valid input format is a URL for this
+  * converion. Overrides the parent array.
+  */
+	protected $_validInputFormats = 'url';
+
+}
