2fc5790dd83eefb6381133ca8a8b6e242e494247
ConfuzzledDuck <jonathon@flutt.co.uk>
2014-06-21 07:48:13 +0000

    Added conversion web to image.

---
+++ Web2Image.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ConvertAPI;
+
+require_once('Abstract2Image.php');
+
+ /**
+  * Extends the ConvertAPI class to convert URLs into image format via
+  * convertapi.com.
+  *
+  * @see http://www.convertapi.com/web-image-api
+  */
+class Web2Image extends Abstract2Image {
+
+ /**
+  * URL of the appropriate convertapi.com API.
+  */
+	protected $_apiUrl = 'http://do.convertapi.com/Web2Image';
+
+ /**
+  * A string indicating that the valid input format is a URL for this
+  * converion. Overrides the parent array.
+  */
+	protected $_validInputFormats = 'url';
+
+}
