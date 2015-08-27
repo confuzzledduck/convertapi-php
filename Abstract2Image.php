0ed969f93b91246e2b62b4e53af9e99de355d5e1
ConfuzzledDuck <jonathon@flutt.co.uk>
2015-08-27 21:05:48 +0000

    Modified parameters to match new image params of web service.

--- Abstract2Image.php
+++ Abstract2Image.php
@@ -21,14 +21,6 @@ abstract class Abstract2Image extends ConvertAPI {
 	public function __set($name, $value) {
 
 		switch ($name) {
-			case 'DocumentTitle': case 'DocumentSubject':
-			case 'DocumentAuthor': case 'DocumentKeywords':
-				if (is_string($value)) {
-					$this->_additionalParameters[$name] = $value;
-				} else {
-					throw new \Exception($name.' must be a string.');
-				}
-			break;
 			case 'OutputFormat':
 				if (is_string($value) && in_array($value, array('png', 'jpg', 'tif'))) {
 					$this->_additionalParameters[$name] = $value;
@@ -36,7 +28,21 @@ abstract class Abstract2Image extends ConvertAPI {
 					throw new \Exception($name.' must be "png", "jpg" or "tif".');
 				}
 			break;
-			case 'AlternativeParser': case 'StoreFile':
+			case 'JpgQuality':
+				if (is_int($value) && $value >= 1 && $value <= 100) {
+					$this->_additionalParameters[$name] = $value;
+				} else {
+					throw new \Exception($name.' must be an integer between 1 and 100.');
+				}
+			break;
+			case 'ImageResolutionH': case 'ImageResolutionV':
+				if (is_int($value) && $value >= 72 && $value <= 1200) {
+					$this->_additionalParameters[$name] = $value;
+				} else {
+					throw new \Exception($name.' must be an integer between 72 and 1200.');
+				}
+			break;
+			case 'StoreFile':
 				if (is_bool($value)) {
 					$this->_additionalParameters[$name] = $value;
 				} else {
@@ -44,10 +50,10 @@ abstract class Abstract2Image extends ConvertAPI {
 				}
 			break;
 			case 'Timeout':
-				if (is_int($value) && $value >= 5 && $value <= 600) {
+				if (is_int($value) && $value >= 5 && $value <= 1200) {
 					$this->_additionalParameters[$name] = $value;
 				} else {
-					throw new \Exception($name.' must be an integer between 5 and 600.');
+					throw new \Exception($name.' must be an integer between 5 and 1200.');
 				}
 			break;
 		}
